55acea922ddd54f70a8ad9bd6ae9c33a35415536
3taem-design <samircmxa@gmail.com>
2026-06-16 10:44:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 function loadGame(gameName) {
     document.getElementById('main-grid').classList.add('hidden');
     document.getElementById('game-wrapper').classList.remove('hidden');
-    
-    // حقن ملف اللعبة ديناميكياً
     const script = document.createElement('script');
     script.src = `games/${gameName}.js`;
+    script.id = 'dynamic-game-script';
     document.body.appendChild(script);
 }
 
 function backToGrid() {
+    const script = document.getElementById('dynamic-game-script');
+    if(script) script.remove();
     document.getElementById('main-grid').classList.remove('hidden');
     document.getElementById('game-wrapper').classList.add('hidden');
-    document.getElementById('game-container').innerHTML = ''; // تنظيف الحاوية
+    document.getElementById('game-container').innerHTML = '';
 }
