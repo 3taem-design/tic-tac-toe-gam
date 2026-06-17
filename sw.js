a5902e601c2acb7d97568bc96ee407b74ac0f387
3taem-design <samircmxa@gmail.com>
2026-06-17 14:45:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,26 @@
-const CACHE_NAME = 'ax-tools-v1';
+const CACHE_NAME = 'ax-tools-v2'; // قمت بتغيير الإصدار لـ v2 ليتم تحديث الكاش
 const ASSETS = [
+    '/',
     'index.html',
     'style.css',
-    'app.js'
+    'app.js',
+    'icons/icon-archive-folder.png',
+    'icons/icon-downloading.png',
+    'icons/icon-email.png',
+    'icons/icon-home.png',
+    'icons/icon-more.png',
+    'icons/icon-numbers.png',
+    'icons/icon-play.png',
+    'icons/icon-settings.png',
+    'icons/icon-tools.png',
+    'icons/icon-trading.png',
+    'icons/icon-user.png'
 ];
 
 // تثبيت الكاش
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-    self.skipWaiting(); // تجبر المتصفح على تفعيل النسخة الجديدة فوراً
+    self.skipWaiting();
 });
 
 // تفعيل النسخة الجديدة وحذف الكاش القديم
